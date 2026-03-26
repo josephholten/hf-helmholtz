1bc3a3907d5f5fb9df2806770849894d2048bb78
Joseph Holten <joseph@holten.com>
2026-03-26 13:55:14 +0100

    better plot script

--- plot.sh
+++ plot.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-gnuplot -p -e "set xlabel 'k'; set ylabel 'Q-O ratio'; set title 'p=1'; set xtics 5; plot 'ratio.dat' w lp pt 1 t 'h\~k^{-1}'"
+gnuplot -p <<EOF
+set xlabel 'k'
+set ylabel 'Q-O ratio'
+set title 'p=1'
+set xtics 5
+plot 'ratio.dat' u 1:2 w lp pt 1 t 'h\~k^{-1}', '' u 1:3 w lp pt 1 t 'h\~k^{-2}'
+EOF
